0d1a70d970b2eb703ff27d57a50dd0d04bc9d19c
Lucas Santos <lucasantos603@gmail.com>
2021-09-08 06:13:37 -0300

    add CR functions
    - Add Strings to list and create a archive json in mobile
    - Read a archive json in device

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,10 +12,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _listaTarefas = ["teste 1", "teste 2", "teste 3"];
+  List _listaTarefas = [];
+
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationSupportDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = "ir ao mercado";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+    // print("Caminho: " + diretorio.path);
+  }
+
+  _lerArquivo() async {
+    try {
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      // e.toString();
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lerArquivo().then((dados) {
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    // _salvarArquivo();
+
+    print("itens " + _listaTarefas.toString());
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de tarefas"),
@@ -51,7 +98,7 @@ class _HomeState extends State<Home> {
             itemCount: _listaTarefas.length,
             itemBuilder: (context, index) {
               return ListTile(
-                title: Text(_listaTarefas[index]),
+                title: Text(_listaTarefas[index]['titulo']),
               );
             },
           ))
